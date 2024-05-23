26936201e6ce5e9a432e6b38538cceb871dc1cb5
Arijit Debnath <itsarijitdebnath@gmail.com>
2024-05-23 23:54:39 +0530

    Removed unnecessary console log

--- src/actions/fetchCricketSeries.ts
+++ src/actions/fetchCricketSeries.ts
@@ -5,6 +5,5 @@ export const fetchCricketSeries = async (): Promise<string[]> => {
     `https://api.cricapi.com/v1/series?apikey=${process.env.CRICKET_API_KEY}&offset=0`
   );
 
-  console.log(response.data);
   return response.data;
 };
